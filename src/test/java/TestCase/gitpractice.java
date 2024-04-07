18a53f6f4edfd4e8c440ae6b22a01aebd71727b9
Your Name <you@example.com>
2024-04-07 07:12:01 +0530

    this is from eclipse

---
+++ src/test/java/TestCase/gitpractice.java
@@ -0,0 +1,13 @@
+package TestCase;
+
+import org.testng.annotations.Test;
+
+public class gitpractice {
+	@Test
+public 	void gitpractice() {
+		System.out.println ("this is git practice");
+	
+	}
+	}
+
+
