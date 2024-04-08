69a7280f119f0aeae6f15f274dba0ebe442e98f9
Your Name <you@example.com>
2024-04-08 11:35:22 +0530

    'changes'

--- src/test/java/TestCase/gitpractice.java
+++ src/test/java/TestCase/gitpractice.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class gitpractice {
 	@Test
 public 	void gitpractice() {
-		System.out.println ("this is git practice");
+		System.out.println ("this is    git practice");
 	
 	}
 	}
